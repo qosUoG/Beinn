5fde77068233e92dbd1beda0c9445a2cdc0be06c
Lomond <placeholder@eng-ad.gla.ac.uk>
2025-02-18 17:42:23 +0000

    streaming stdout instead

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,10 +1,31 @@
 import { randomUUIDv7, serve, type ServerWebSocket, type Subprocess } from "bun"
 import { type ToBackendMessage, type ToFrontendMessage } from "shared-types"
 import { getDirectoryInfo } from "./handlers/set_directory";
+import { Readable } from 'node:stream'
 
 const ws_map: Map<string, ServerWebSocket<undefined>> = new Map()
 
 const py_map: Map<string, Subprocess> = new Map()
+
+process.on("SIGINT", () => {
+    // Gracefully stop pyproc
+    py_map.entries().forEach(([k, v]) => {
+        v.kill()
+        py_map.delete(k)
+        console.log(`py proc ${k} killed`)
+    })
+
+    // Gracefully stop websocket
+    ws_map.entries().forEach(([k, v]) => {
+        v.close()
+        ws_map.delete(k)
+        console.log(`ws connection ${k} closed`)
+    })
+
+    console.log("Backend Process killed")
+
+    process.exit()
+})
 
 serve<undefined>({
     port: 4000,
@@ -66,8 +87,24 @@ serve<undefined>({
 
                     const pyproc = Bun.spawn(["uv", "run", msg.payload.script_name], {
                         cwd: msg.payload.cwd,
-                        stdout: "inherit"
                     })
+
+                    async function logPyProc() {
+                        let out = ""
+                        const stdout = Readable.fromWeb(pyproc.stdout)
+                        while (true) {
+                            console.log("here")
+
+                            // out = await new Response(pyproc.stdout).text()
+                            for await (const x of stdout) {
+
+                                console.log(`STDOUT: ${x}`)
+                            }
+
+                        }
+                    }
+
+                    logPyProc()
 
                     const id = randomUUIDv7()
                     py_map.set(id, pyproc)
