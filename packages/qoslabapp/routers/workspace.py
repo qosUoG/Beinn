41c97665b4ed33d57ae636266407ce8a4a77474c
harrylaulau <dev+github@harrylaulau.me>
2025-03-03 19:37:42 +0000

    update app

--- packages/qoslabapp/routers/workspace.py
+++ packages/qoslabapp/routers/workspace.py
@@ -74,12 +74,13 @@ async def available_equipments():
 
         def get_equipments(name: str):
             # First check the name is importable
-            if importlib.util.find_spec(name):
+            if spec := importlib.util.find_spec(name):
                 if name.startswith("msvcrt"):
                     print(name)
-                for [p, module] in inspect.getmembers(importlib.import_module(name)):
-                    if hasattr(module, "equipment_params"):
-                        equipments.append({"module_name": p, "equipment_name": p})
+                    print(spec)
+                # for [p, module] in inspect.getmembers(importlib.import_module(name)):
+                #     if hasattr(module, "equipment_params"):
+                #         equipments.append({"module_name": p, "equipment_name": p})
 
         get_equipments(package.name)
 
