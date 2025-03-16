3d02ce2860b74826137223698aa66ac2d4e9ce26
harrylaulau <dev+github@harrylaulau.me>
2025-03-16 21:10:22 +0000

    updates

--- example/experiment/main.py
+++ example/experiment/main.py
@@ -13,8 +13,45 @@ def main():
 
 
 if __name__ == "__main__":
-    x = 1
-    print("{" + f"loop_count: {x}" + "}\n\n")
+
+    class EquipmentProxy:
+        # Threading.Lock for thread safety access of Equipment
+
+        def __init__(self):
+            super().__setattr__("_lock", Lock())
+            super().__setattr__("_equipment", 1)
+
+        # For Type safety
+        @property
+        def params(self):
+            with super()._lock:
+                return super()._equipment
+
+        @params.setter
+        def params(self, value: int):
+            with super()._lock:
+                super()._equipment = value
+
+        # Proxy to all underlying parameters
+        def __getattr__(self, key: str):
+            with super()._lock:
+                attr = getattr(super()._equipment, key)
+                if callable(attr):
+
+                    def wrap(*args, **kwargs):
+                        with super()._lock:
+                            return attr(*args, **kwargs)
+
+                    return wrap
+
+                return attr
+
+        def __setattr__(self, key: str, value: Any):
+            with super()._lock:
+                setattr(super()._equipment, key, value)
+
+    e = EquipmentProxy()
+
     # def inside():
     #     warnings.filterwarnings("error")
 
--- packages/qoslabapp/lib/proxies/equipment.py
+++ packages/qoslabapp/lib/proxies/equipment.py
@@ -10,28 +10,28 @@ class EquipmentProxy:
     # Threading.Lock for thread safety access of Equipment
 
     def __init__(self, eCls: type[EquipmentABC]):
-        self._equipment = eCls()
-        self._lock = Lock()
+        super().__setattr__("_lock", Lock())
+        super().__setattr__("_equipment", eCls())
 
     # For Type safety
     @property
     def params(self) -> Params:
-        with self._lock:
-            return self._equipment.params
+        with super()._lock:
+            return super()._equipment.params
 
     @params.setter
     def params(self, params: Params):
-        with self._lock:
-            self._equipment.params = params
+        with super()._lock:
+            super()._equipment.params = params
 
     # Proxy to all underlying parameters
     def __getattr__(self, key: str):
-        with self._lock:
-            attr = getattr(self._equipment, key)
+        with super()._lock:
+            attr = getattr(super()._equipment, key)
             if callable(attr):
 
                 def wrap(*args, **kwargs):
-                    with self._lock:
+                    with super()._lock:
                         return attr(*args, **kwargs)
 
                 return wrap
@@ -39,5 +39,5 @@ class EquipmentProxy:
             return attr
 
     def __setattr__(self, key: str, value: Any):
-        with self._lock:
-            setattr(self._equipment, key, value)
+        with super()._lock:
+            setattr(super()._equipment, key, value)
