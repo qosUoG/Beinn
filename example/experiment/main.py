0b706bcff8d8e9f804a218b804b16340bb8cce6b
harrylaulau <dev+github@harrylaulau.me>
2025-03-16 13:45:32 +0000

    updates

--- example/experiment/main.py
+++ example/experiment/main.py
@@ -13,53 +13,8 @@ def main():
 
 
 if __name__ == "__main__":
-
-    class A:
-        x = 1
-
-        @classmethod
-        def getx(cls):
-            return cls.x
-
-        def echo(self, value: str):
-            return value
-
-        pass
-
-    @dataclass
-    class B:
-        a = A()
-        _lock = Lock()
-
-        def __getattr__(self, key: str):
-            attr = getattr(self.a, key)
-            if callable(attr):
-
-                def wrap(*args, **kwargs):
-                    with self._lock:
-                        return attr(*args, **kwargs)
-
-                return wrap
-
-            return attr
-
-        def __setattr__(self, key: str, value: Any):
-            return setattr(self.a, key, value)
-
-    b = B()
-
-    b.x = 2
-
-    print(b.x)
-    print(b.a.x)
-    print(b.echo)
-    print(b.echo("hi"))
-    print(b.a.echo)
-    print(b.a.echo("hhhi"))
-
-    b.sth = "wow"
-    print(b.sth)
-    print(b.a.sth)
+    x = 1
+    print(f"{{loop_count: {x}\}}\n\n")
     # def inside():
     #     warnings.filterwarnings("error")
 
