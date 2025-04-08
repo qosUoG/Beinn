2e64c14a16e097b2b71a892e0e16ab97fd5cd243
harrylaulau <dev+github@harrylaulau.me>
2025-04-08 14:59:58 +0100

    cleanup after merging deno

---
+++ packages/backend-bun/build.ts
@@ -0,0 +1,3 @@
+import { $ } from "bun";
+
+await $`bun build  --compile ./index.ts ./static/**/*.jss ./static/**/*.css    ./static/index.html --outfile out/qoslab`
--- packages/backend/build.ts
+++
@@ -1,3 +0,0 @@
-import { $ } from "bun";
-
-await $`bun build  --compile ./index.ts ./static/**/*.jss ./static/**/*.css    ./static/index.html --outfile qoslab`
