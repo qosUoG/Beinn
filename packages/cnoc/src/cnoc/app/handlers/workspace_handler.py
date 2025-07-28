755552581edf452638072809af64519cc8b52167
harrylaulau <dev+github@harrylaulau.me>
2025-07-28 12:55:22 +0100

    update cnoc

--- packages/cnoc/src/cnoc/app/handlers/workspace_handler.py
+++ packages/cnoc/src/cnoc/app/handlers/workspace_handler.py
@@ -25,7 +25,7 @@ def eeImports[T: type[ExperimentABC] | type[EquipmentABC]](
 
     # Check all possible paths
     packages.append(".")
-    for package in pkgutil.iter_modules(packages):
+    for package in pkgutil.walk_packages(packages):
         try:
             for [cls, clsT] in inspect.getmembers(
                 importlib.import_module(package), inspect.isclass
