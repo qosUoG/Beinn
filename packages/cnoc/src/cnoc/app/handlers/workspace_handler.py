a72ab15f05c75db9e1c10393d7a39f20ee0902ad
harrylaulau <dev+github@harrylaulau.me>
2025-08-02 21:31:10 +0100

    update

--- packages/cnoc/src/cnoc/app/handlers/workspace_handler.py
+++ packages/cnoc/src/cnoc/app/handlers/workspace_handler.py
@@ -28,28 +28,31 @@ async def workspaceHandler(ws: ServerConnection):
     Foundation.workspace_ws = ws
     print("Connected to workspace", flush=True)
     async for message in ws:
-        req = json.loads(message)
+        try:
+            req = json.loads(message)
+            command: str = req["command"]
 
-        command: str = req["command"]
-
-        if command in eeHandlers.keys():
-            await eeHandlers[command](ws, req["value"])
-            continue
-
-        if command.startswith("experiment:"):
-            name = req["value"]["name"]
-            if name not in Experiments.instances:
-                print(f"Experiment {name} does not exist", flush=True)
+            if command in eeHandlers.keys():
+                await eeHandlers[command](ws, req["value"])
                 continue
 
-            match command.split(":")[1]:
-                case "start":
-                    Experiments.instances[name].instance._cnoc_start()
-                case "pause":
-                    Experiments.instances[name].instance._cnoc_pause()
-                case "stop":
-                    Experiments.instances[name].instance._cnoc_stop()
-                case "continue":
-                    Experiments.instances[name].instance._cnoc_continue()
+            if command.startswith("experiment:"):
+                name = req["value"]["name"]
+                if name not in Experiments.instances:
+                    print(f"Experiment {name} does not exist", flush=True)
+                    continue
 
+                match command.split(":")[1]:
+                    case "start":
+                        Experiments.instances[name].instance._cnoc_start()
+                    case "pause":
+                        Experiments.instances[name].instance._cnoc_pause()
+                    case "stop":
+                        Experiments.instances[name].instance._cnoc_stop()
+                    case "continue":
+                        Experiments.instances[name].instance._cnoc_continue()
+
+                continue
+        except Exception as e:
+            print(f"Exception in workspace handler: {e}", flush=True)
             continue
