5bf2ee63818da1eb459738aefc54b099cb95c281
harrylaulau <dev+github@harrylaulau.me>
2025-10-12 21:14:46 +0100

    update cnoc

--- packages/cnoc/src/cnoc/scripts/params.py
+++ packages/cnoc/src/cnoc/scripts/params.py
@@ -2,10 +2,21 @@ import importlib
 import json
 import sys
 
+from ..public.params import cnoc_params2Dict
+
 
 def main():
     module = importlib.import_module(sys.argv[1])
     module = importlib.reload(module)
 
     instance = getattr(module, sys.argv[2])()
-    print(json.dumps(instance.params))
+
+    print(
+        json.dumps(
+            {
+                "module": sys.argv[1],
+                "cls": sys.argv[2],
+                "params": cnoc_params2Dict(instance.params),  # type: ignore
+            }
+        )
+    )
